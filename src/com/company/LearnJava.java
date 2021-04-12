c6793d5399899e180f51419c58248276a13bed99
SandraAnghel <sandra.anghel2701@gmail.com>
2021-04-12 13:12:36 +0100

    Syntax and comments

---
+++ src/com/company/LearnJava.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class LearnJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // First java program, prints Hello World
+
+        /* This is a java program.
+           It prints Java is Fun!
+         */
+
+        System.out.println("Java is Fun!"); // prints Java is Fun!
+    }
+}

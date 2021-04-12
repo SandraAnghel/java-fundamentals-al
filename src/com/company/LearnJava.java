0c23bee6543242fe4067fd242a79cc8261a520c2
SandraAnghel <sandra.anghel2701@gmail.com>
2021-04-12 13:30:50 +0100

    Working with variables

--- src/com/company/LearnJava.java
+++ src/com/company/LearnJava.java
@@ -10,5 +10,16 @@ public class LearnJava {
          */
 
         System.out.println("Java is Fun!"); // prints Java is Fun!
+
+        Variables v = new Variables();
+        System.out.println("This is variable a from Variables class: " + v.a);
+        System.out.println("This is variable age from Variables class: " + v.age);
+        System.out.println("This is variable superAwesomeVariableIMade from Variables class: " + v.superAwesomeVariableIMade);
+
+        Variables firstPerson = new Variables();
+        System.out.println("The details of the first winner is:\n " +
+                "Name: " + firstPerson.firstName + " " + firstPerson.lastName + "\n" +
+                "Address: " + firstPerson.address  + "\n" +
+                "Phone number: " + firstPerson.phoneNumber);
     }
 }
---
+++ src/com/company/Variables.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Variables {
+
+    int a = 5;
+    int age = 19;
+    int superAwesomeVariableIMade = 9000;
+    String firstName = "Smith";
+    String lastName = "Sandra";
+    String address = "Flat 3, Aegean House, Harsfold Close, Rustington";
+    String phoneNumber = "0798752111";
+
+
+
+}

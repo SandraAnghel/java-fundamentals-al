89e5aaccc7b6a024c585fb1b35d4f56d750609e2
SandraAnghel <sandra.anghel2701@gmail.com>
2021-04-13 13:53:13 +0100

    Working with for loops

---
+++ src/com/company/ForLoops.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class ForLoops {
+
+    public static void forLoopExamples() {
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println("This is my first for loop!");
+        }
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
+
+        String [] cars = {"bmw", "ford", "toyota"};
+
+        for(int i = 0; i < 3; i++){
+            System.out.println(cars[i]);
+        }
+
+        for(String c : cars) {
+            System.out.println(c);
+        }
+
+        String s = "Magical ponies";
+
+        for(int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+        }
+
+        
+    }
+}
